15c4111a86d65d2ab4c2dcb6389087938be23a40
Marlon Zayro Arias Vargas <zayro8905@gmail.com>
2017-07-14 10:19:40 -0500

    solo se pensara dejar las librerias de las clases

--- config/access.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("host", "localhost");
-define("user", "marlon");
-define("pass", "zayro");
-define("database", "acceso");
-define("typeDatabase", "mysql");
-define("port", "3307");
--- config/sesion.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-#iniciar sessiones
-session_start();
