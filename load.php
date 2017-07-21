8158765d3a88eb212409864a0a73e862d99e35a0
Marlon Zayro Arias Vargas <zayro8905@gmail.com>
2017-07-20 20:00:26 -0500

    se elimina archivo innecesarios

--- load.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__.'/config/ini.php';
-require_once __DIR__.'/config/access.php';
-require_once __DIR__.'/config/cors.php';
-require_once __DIR__.'/config/sesion.php';
-
-require_once __DIR__.'/vendor/autoload.php';
