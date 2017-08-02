cce50e36153a5059515b2bc623eb00ad9260d3ad
Marlon Zayro Arias Vargas <zayro8905@gmail.com>
2017-08-02 14:27:09 -0500

    se elimina archivos innecesarios

--- library/mail.class.php
+++ library/mail.class.php
@@ -1,63 +1,62 @@
 <?php
+#define('BASE_URL', 'http://' . filter_input(INPUT_SERVER, 'SERVER_NAME', FILTER_SANITIZE_STRING) . '/ambiensqpro/');
 
 class Correo
 {
     public function __construct()
     {
+        $this->oMail = new PHPMailer(true);
+        $this->oMail->isSMTP();
+        $this->oMail->Mailer = 'smtp';
+        $this->oMail->SMTPSecure = 'ssl';
+        $this->oMail->Host = 'ssl://smtp.gmail.com';
+        $this->oMail->Port = 465;
+        $this->oMail->SMTPAuth = true;
+        $this->oMail->IsHTML(true);
+        $this->oMail->Username = 'no-reply@ambiensq.com';
+        $this->oMail->Password = '41t3_ambiensQ';
+        $this->oMail->Timeout = 10;
+        $this->oMail->From = $this->oMail->Username;
     }
 
-    public static function configureMail()
-    {
-        $oMail = new PHPMailer();
-        $oMail->PluginDir = "";
-        $oMail->Mailer = "";
-        $oMail->Host = "ssl://smtp.gmail.com";
-        $oMail->Port = "465";
-        $oMail->SMTPAuth = true;
-        $oMail->Username = "no-reply@ambiensq.com";
-        $oMail->password = "41t3_ambiensQ";
-
-        return $oMail;
-    }
-
-    public static function enviarCorreo(string $sAsunto, string $sMensaje, string $sNombreOrigen, array $aInbox)
+    public function enviarCorreo(string $sAsunto, string $sMensaje, string $sNombreOrigen, array $aInbox)
     {
         try {
-            $oMail = Correo::configureMail();
-            $oMail->From = $oMail->Username;
-            $oMail->FromName = $sNombreOrigen;
-            $oMail->Timeout = 10;
-            $oMail->Subject = $sAsunto;
-            $oMail->Body = $sMensaje;
-            $oMail->AltBody = $sMensaje;
+            $this->oMail->FromName = $sNombreOrigen;
+            $this->oMail->Subject = $sAsunto;
+            $this->oMail->Body = $sMensaje;
+            $this->oMail->AltBody = $sMensaje;
 
             foreach ($aInbox as $sMail) {
-                $oMail->AddAddress($sMail);
+                $this->oMail->AddAddress($sMail);
             }
 
-            $oMail->Send();
-            $oMail->ClearAddresses();
-            $oMail->ClearAttachments();
+            $this->oMail->Send();
+            $this->oMail->ClearAddresses();
+            $this->oMail->ClearAttachments();
         } catch (phpmailerException $e) {
             echo $e->errorMessage();
         } catch (Exception $e) {
             echo $e->getMessage();
         }
     }
+    
 
-    public static function correoAlertaRegistroUsuario(array $aDataVars, array $aInbox)
+    public function correoAlertaRegistroUsuario(array $aDataVars, array $aInbox, string $sUrl)
     {
         $sPlantilla = str_replace(
             array_keys($aDataVars),
             array_values($aDataVars),
-            file_get_contents(BASE_URL . 'template/correoAlertaRegistroUsuario.html')
+            file_get_contents($sUrl)
         );
 
-        Correo::enviarCorreo(
-            'Su cuenta de ambiensqpro ha sido registrada',
+        $this->enviarCorreo(
+            'Su cuenta de  ha sido registrada',
             $sPlantilla,
-            'ambiensqpro',
+            'software',
             $aInbox
         );
     }
 }
+
+    #$this->oCorreo->correoAlertaRegistroUsuario(array('$_sNombreUsuario_' => $this->oDataRequest->nombre, '$_sApellidoUsuario_' => $this->oDataRequest->apellido, '$_sCorreoUsuario_' => $this->oDataRequest->correo, '$_sContraseniaUsuario_' => $sPassword), array($this->oDataRequest->correo));
