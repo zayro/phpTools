a5b45e8f793685027b28a9be744e555477f4d2b3
Marlon Zayro Arias Vargas <zayro8905@gmail.com>
2017-08-03 18:06:22 -0500

    se suben nuevos test de librerias php

--- test/log/vendor.log.php
+++ test/log/vendor.log.php
@@ -4,10 +4,9 @@ require '../../vendor/autoload.php';
 
 $logger = new PHPTools\PHPErrorLog\PHPErrorLog();
 
+print $logger->write('Prueba de write PEL_INFO...', PEL_INFO, realpath('/tmp/dev.txt'));
+print $logger->write('probando...', PEL_INFO, '/tmp/dev.txt');
+
 $logger->write('Prueba de write...');
 
 $logger->write('Prueba de write PEL_INFO...', PEL_INFO);
-
-print $logger->write('Prueba de write PEL_INFO...', PEL_INFO, realpath('dev.log'));
-
-print $logger->write('probando...', PEL_DEBUG, '/var/www/dev.log');
